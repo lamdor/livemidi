27c1415f41e43e3d6079eff3f889348df62bf1b8
Luke Amdor <luke@absoluterubbish.net>
2008-06-29 19:08:27 -0500

    Added #note_off method

--- lib/livemidi/livemidi.rb
+++ lib/livemidi/livemidi.rb
@@ -1,8 +1,13 @@
 class LiveMidi
   ON = 0x90
+  OFF = 0x80
 
   def note_on(channel, note, velocity = 64)
     message(ON | channel, note, velocity)
+  end
+  
+  def note_off(channel, note, velocity = 64)
+    message(OFF | channel, note, velocity)
   end
   
 end
