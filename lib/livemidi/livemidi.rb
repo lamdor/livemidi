5ad2def3bc4180a1d04810a196a04e5ca047448f
Luke Amdor <luke@absoluterubbish.net>
2008-06-30 18:18:07 -0500

    made program change a constant

--- lib/livemidi/livemidi.rb
+++ lib/livemidi/livemidi.rb
@@ -1,6 +1,7 @@
 class LiveMidi
   ON = 0x90
   OFF = 0x80
+  PROGRAM_CHANGE = 0xC0
 
   def note_on(channel, note, velocity = 64)
     message(ON | channel, note, velocity)
@@ -11,7 +12,7 @@ class LiveMidi
   end
 
   def program_change(channel, preset)
-    message(0xC0 | channel, preset)
+    message(PROGRAM_CHANGE | channel, preset)
   end
   
 end
