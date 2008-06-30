324af32919093b8964e560c1f55164b860f70054
Luke Amdor <luke@absoluterubbish.net>
2008-06-29 21:40:46 -0500

    Added lined method to open and close

--- lib/livemidi/livemidi_windows.rb
+++ lib/livemidi/livemidi_windows.rb
@@ -9,4 +9,13 @@ class LiveMidi
     extern "int midiOutClose(int)"
     extern "int midiOutShortMsg(int,int)"
   end
+
+  def open
+    @device = DL.malloc(DL.sizeof('I'))
+    C.midiOutOpen(@device, -1, 0, 0, 0)
+  end
+
+  def close
+    C.midiOutClose(@device.ptr.to_i)
+  end
 end
--- spec/livemidi/livemidi_windows_spec.rb
+++ spec/livemidi/livemidi_windows_spec.rb
@@ -9,13 +9,34 @@ end
 describe "LiveMidi on Windows" do
 
   describe LiveMidi::C do
-    
     it "should load winmm libraries, and link to the 'int midiOut*' methods" do
       LiveMidi::C.should_receive(:dlload).with("winmm")
       LiveMidi::C.should_receive(:extern).with("int midiOutOpen(HDMIDIOUT*, int, int, int, int)")
       LiveMidi::C.should_receive(:extern).with("int midiOutClose(int)")
       LiveMidi::C.should_receive(:extern).with("int midiOutShortMsg(int,int)")
       load 'livemidi/livemidi_windows.rb'
+    end
+  end
+
+  before :each do
+    @livemidi = LiveMidi.new
+    @mock_device = mock("device")
+  end
+
+  describe "#open" do
+    it "should call C.midiOutOpen with a newly allocated device pointer to an integer" do
+      DL.stub!(:malloc).and_return(@mock_device)
+      LiveMidi::C.should_receive(:midiOutOpen).with(@mock_device, -1, 0, 0, 0)
+      @livemidi.open
+    end
+  end
+
+  describe "#close" do
+    it "should call C.midiOutClose with the device it was created with" do
+      @livemidi.instance_variable_set(:@device, @mock_device)
+      @mock_device.should_receive(:ptr).and_return("100")
+      LiveMidi::C.should_receive(:midiOutClose).with(100)
+      @livemidi.close
     end
   end
   
